d818daf45b9a1c45a2904f00e3367e3e8d3acead
coolcatcher126 <86249897+coolcatcher126@users.noreply.github.com>
2025-06-16 23:44:36 +0100

    Initial Commit

---
+++ src/main/java/io/github/coolcatcher126/ferrocerium/InvasionFerroceriumClient.java
@@ -0,0 +1,9 @@
+package io.github.coolcatcher126.ferrocerium;
+import net.fabricmc.api.ClientModInitializer;
+
+public class InvasionFerroceriumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
