953c448d7f87cacfae4ea6f32d6afad4601bad82
coolcatcher126 <86249897+coolcatcher126@users.noreply.github.com>
2025-08-14 20:04:23 +0100

    Continue work

---
+++ src/main/java/io/github/coolcatcher126/ferrocerium/components/InvasionFerroceriumComponents.java
@@ -0,0 +1,19 @@
+package io.github.coolcatcher126.ferrocerium.components;
+
+import net.minecraft.util.Identifier;
+import org.ladysnake.cca.api.v3.component.ComponentKey;
+import org.ladysnake.cca.api.v3.component.ComponentRegistry;
+import org.ladysnake.cca.api.v3.world.WorldComponentFactoryRegistry;
+import org.ladysnake.cca.api.v3.world.WorldComponentInitializer;
+
+public class InvasionFerroceriumComponents implements WorldComponentInitializer {
+    public static final ComponentKey<InvasionLevelComponent> INVASION_LEVEL = ComponentRegistry.getOrCreate(
+            Identifier.of("invasion-ferrocerium", "invasion_state"),
+            InvasionLevelComponent.class
+    );
+
+    @Override
+    public void registerWorldComponentFactories(WorldComponentFactoryRegistry registry) {
+        registry.register(INVASION_LEVEL, InvasionLevelComponent::new);
+    }
+}
