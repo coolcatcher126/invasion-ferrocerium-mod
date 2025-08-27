352464431bcbfc8b1b60688e2323b094dd95c6f5
Keenan <86249897+coolcatcher126@users.noreply.github.com>
2025-08-27 10:04:58 +0100

    Make invasion state affect spawns (#8)

--- src/main/java/io/github/coolcatcher126/ferrocerium/world/gen/ModEntitySpawn.java
+++ src/main/java/io/github/coolcatcher126/ferrocerium/world/gen/ModEntitySpawn.java
@@ -1,6 +1,8 @@
 package io.github.coolcatcher126.ferrocerium.world.gen;
 
 import io.github.coolcatcher126.ferrocerium.entity.ModEntities;
+import io.github.coolcatcher126.ferrocerium.entity.custom.AntScoutBotEntity;
+import io.github.coolcatcher126.ferrocerium.entity.custom.AntSoldierBotEntity;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.entity.SpawnGroup;
@@ -11,9 +13,13 @@ import net.minecraft.world.Heightmap;
 
 public class ModEntitySpawn {
     public static void addSpawns() {
-        BiomeModifications.addSpawn(BiomeSelectors.foundInOverworld(),
+        BiomeModifications.addSpawn(BiomeSelectors.all(),
                 SpawnGroup.MONSTER, ModEntities.ANT_SCOUT_BOT, 10, 1, 1);
 
-        SpawnRestriction.register(ModEntities.ANT_SCOUT_BOT, SpawnLocationTypes.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HostileEntity::canSpawnInDark);
+        BiomeModifications.addSpawn(BiomeSelectors.all(),
+                SpawnGroup.MONSTER, ModEntities.ANT_SOLDIER_BOT, 10, 1, 1);
+
+        SpawnRestriction.register(ModEntities.ANT_SCOUT_BOT, SpawnLocationTypes.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AntScoutBotEntity::canSpawnInDark);
+        SpawnRestriction.register(ModEntities.ANT_SOLDIER_BOT, SpawnLocationTypes.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AntSoldierBotEntity::canSpawnInDark);
     }
 }
