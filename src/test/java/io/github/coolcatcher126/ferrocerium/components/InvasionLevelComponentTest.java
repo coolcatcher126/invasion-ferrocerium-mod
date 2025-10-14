a191a4b30c9be33fc0e9f2feb942f62e3ba49382
coolcatcher126 <86249897+coolcatcher126@users.noreply.github.com>
2025-10-14 16:05:40 +0100

    Add tests

---
+++ src/test/java/io/github/coolcatcher126/ferrocerium/components/InvasionLevelComponentTest.java
@@ -0,0 +1,21 @@
+package io.github.coolcatcher126.ferrocerium.components;
+
+import net.minecraft.Bootstrap;
+import net.minecraft.SharedConstants;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+public class InvasionLevelComponentTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        SharedConstants.createGameVersion();
+        Bootstrap.initialize();
+    }
+
+    @Test
+    void testAlienBaseSaveFromAlienBase(){
+
+    }
+}
