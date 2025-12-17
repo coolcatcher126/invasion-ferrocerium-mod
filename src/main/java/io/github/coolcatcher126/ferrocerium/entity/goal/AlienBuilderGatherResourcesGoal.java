ba2a9c7baa232e535ab0dc21cb2cbd95f4e7a859
coolcatcher126 <86249897+coolcatcher126@users.noreply.github.com>
2025-12-17 12:04:14 +0000

    Update AlienBuilderGatherResourcesGoal.java
    Protects the pillaring blocks from being destroyed before the alien builder has finished with them.
    TODO: destroy the pillar after the bot is finished

--- src/main/java/io/github/coolcatcher126/ferrocerium/entity/goal/AlienBuilderGatherResourcesGoal.java
+++ src/main/java/io/github/coolcatcher126/ferrocerium/entity/goal/AlienBuilderGatherResourcesGoal.java
@@ -1,6 +1,7 @@
 package io.github.coolcatcher126.ferrocerium.entity.goal;
 
 import io.github.coolcatcher126.ferrocerium.entity.custom.AlienBuilderBotEntity;
+import io.github.coolcatcher126.ferrocerium.resources.Vein;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -28,7 +29,9 @@ public class AlienBuilderGatherResourcesGoal extends Goal {
 
     private Path path;
 
-    private BlockPos blockToCollect;
+    private Vein pillar;
+    private Vein vein;
+    private int blockToCollect;
 
     public AlienBuilderGatherResourcesGoal(AlienBuilderBotEntity alienBuilderBot, double speed){
         this.alienBuilderBot = alienBuilderBot;
@@ -41,21 +44,26 @@ public class AlienBuilderGatherResourcesGoal extends Goal {
         if (alienBuilderBot.getBase() == null || alienBuilderBot.getVein() == null || alienBuilderBot.getVein().size() == 0) {
             return false;
         }
-        else{
-            blockToCollect = alienBuilderBot.getVein().getBottom();
-            this.path = this.alienBuilderBot.getNavigation().findPathTo(blockToCollect, 0);
-            return this.path != null || this.alienBuilderBot.getBlockPos().getSquaredDistance(blockToCollect) < SQR_REACH_RANGE;
+        else {
+            if (pillar != null) {
+                vein = pillar;
+                pillar = null;
+            }
+            vein = alienBuilderBot.getVein();
+            blockToCollect = 0;
+            this.path = this.alienBuilderBot.getNavigation().findPathTo(vein.get(blockToCollect), 0);
+            return this.path != null || this.alienBuilderBot.getBlockPos().getSquaredDistance(vein.get(blockToCollect)) < SQR_REACH_RANGE;
         }
     }
 
     @Override
     public boolean shouldContinue() {
-        if (alienBuilderBot.getVein().size() == 0) {
+        if (vein.size() == 0) {
             return false;
         }
-        blockToCollect = alienBuilderBot.getVein().getBottom();
-        this.path = this.alienBuilderBot.getNavigation().findPathTo(blockToCollect, 0);
-        return this.path != null || this.alienBuilderBot.getBlockPos().getSquaredDistance(blockToCollect) < SQR_REACH_RANGE;
+        blockToCollect = 0;
+        this.path = this.alienBuilderBot.getNavigation().findPathTo(vein.get(blockToCollect), 0);
+        return this.path != null || this.alienBuilderBot.getBlockPos().getSquaredDistance(vein.get(blockToCollect)) < SQR_REACH_RANGE;
     }
 
     @Override
@@ -67,22 +75,22 @@ public class AlienBuilderGatherResourcesGoal extends Goal {
     @Override
     public void stop() {
         alienBuilderBot.setGathering(false);
-        this.alienBuilderBot.setVein(null);
+        vein = null;
         this.alienBuilderBot.getNavigation().stop();
     }
 
     public void tick(){
-        if (alienBuilderBot.getVein().size() == 0) {
+        if (vein.size() == 0) {
             return;
         }
-        blockToCollect = alienBuilderBot.getVein().getBottom();
-        if (blockToCollect != null){
-             if (this.alienBuilderBot.getBlockPos().getSquaredDistance(blockToCollect) < SQR_REACH_RANGE) {
-                this.alienBuilderBot.getLookControl().lookAt(blockToCollect.toCenterPos());
+        blockToCollect = 0;
+        if (vein.get(blockToCollect) != null){
+             if (this.alienBuilderBot.getBlockPos().getSquaredDistance(vein.get(blockToCollect)) < SQR_REACH_RANGE) {
+                this.alienBuilderBot.getLookControl().lookAt(vein.get(blockToCollect).toCenterPos());
                 if (this.alienBuilderBot.getWorld().isClient()){
                     if (countTicksToBreak >= 0) {
                         countTicksToBreak--;
-                        this.alienBuilderBot.getWorld().setBlockBreakingInfo(this.alienBuilderBot.getId(), blockToCollect, Math.round((MAX_BREAK_TICKS - countTicksToBreak) / (float) MAX_BREAK_TICKS * 10));
+                        this.alienBuilderBot.getWorld().setBlockBreakingInfo(this.alienBuilderBot.getId(), vein.get(blockToCollect), Math.round((MAX_BREAK_TICKS - countTicksToBreak) / (float) MAX_BREAK_TICKS * 10));
                         return;
                     }
                     countTicksToBreak = MAX_BREAK_TICKS;
@@ -95,8 +103,8 @@ public class AlienBuilderGatherResourcesGoal extends Goal {
                 } else {
                     countTicksToBreak = MAX_BREAK_TICKS;
                     //Make the bot  mine it as well as all/many other adjacent blocks of the same type.
-                    if (this.alienBuilderBot.getWorld().breakBlock(blockToCollect, true)){
-                        this.alienBuilderBot.getWorld().emitGameEvent(GameEvent.BLOCK_DESTROY, blockToCollect, GameEvent.Emitter.of(this.alienBuilderBot));
+                    if (this.alienBuilderBot.getWorld().breakBlock(vein.get(blockToCollect), true)){
+                        this.alienBuilderBot.getWorld().emitGameEvent(GameEvent.BLOCK_DESTROY, vein.get(blockToCollect), GameEvent.Emitter.of(this.alienBuilderBot));
                         alienBuilderBot.getVein().remove(blockToCollect);
                     }
                 }
@@ -105,16 +113,22 @@ public class AlienBuilderGatherResourcesGoal extends Goal {
                  boolean canMove = this.alienBuilderBot.getNavigation().startMovingAlong(this.path, this.speed);
                  if (!canMove) {
                      BlockPos botPos = this.alienBuilderBot.getBlockPos();
-                     if (blockToCollect.getY() - botPos.getY() > 3) {
+                     if (vein.get(blockToCollect).getY() - botPos.getY() > 3) {
                          //If the block is too high: pillar up.
                          ItemStack stack = this.alienBuilderBot.getMainHandStack();
                          Block block = Block.getBlockFromItem(stack.getItem());
                          if (block != Blocks.AIR) {
+                             if (pillar == null){
+                                 pillar = new Vein();
+                             }
                              this.alienBuilderBot.setJumping(true);
                              this.alienBuilderBot.jump();
                              BlockState stateFromComponent = block.getDefaultState();
-                             this.alienBuilderBot.getWorld().setBlockState(botPos/*.add(0,1,0)*/, stateFromComponent);
+                             this.alienBuilderBot.getWorld().setBlockState(botPos, stateFromComponent);
                              stack.decrement(1);
+                             pillar.add(botPos);
+                             vein.remove(botPos);
+                             this.alienBuilderBot.setVein(vein);
                          }
                      }
                  }
